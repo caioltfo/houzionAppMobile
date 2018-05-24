99519ce24dc775aacd6e83795189299edcecdd82
caioltfo <caioltfo@outlook.com>
2018-05-23 22:53:35 -0300

    atualizacao-home-disconectAmbientes

--- src/pages/ambientes/ambientes.ts
+++ src/pages/ambientes/ambientes.ts
@@ -32,7 +32,6 @@ export class AmbientesPage {
           {
             
             srcImg:"",
-            subtitleCard:"",
             titleCard:"",
             status:0,
             color:"#ff0000"
@@ -46,20 +45,18 @@ export class AmbientesPage {
     
     
     // dados que iram vir do banco
-    this.dados_ambiente.ambiente[0].componente[0].titleCard = "Luzes do Escritório"
+    this.dados_ambiente.ambiente[0].componente[0].titleCard = "Luzes"
     this.dados_ambiente.ambiente[0].nome = "Sala";
     this.dados_ambiente.ambiente[0].temperatura = 20;
     this.dados_ambiente.ambiente[0].umidade = 80;
     this.dados_ambiente.ambiente[0].qttdPortasAbertas = 1;
     this.dados_ambiente.ambiente[0].qttdPortasFechadas = 7;
-    this.dados_ambiente.ambiente[0].componente[0].srcImg = "assets/imgs/luzes.jpeg";
+    this.dados_ambiente.ambiente[0].componente[0].srcImg = "assets/imgs/light.png";
     if(this.dados_ambiente.ambiente[0].componente[0].status = 0)
     {
-      this.dados_ambiente.ambiente[0].componente[0].subtitleCard = "Desativado";
       this.dados_ambiente.ambiente[0].componente[0].color = "#ff0000";
     }else
     {
-      this.dados_ambiente.ambiente[0].componente[0].subtitleCard = "Ativado";
       this.dados_ambiente.ambiente[0].componente[0].color = "#00ff00";
     };
 
